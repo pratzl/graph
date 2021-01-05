5a8cc3e4db5c5bdc27003a2dd1a936ed7444efe2
Phillip Ratzloff <pratzl@outlook.com>
2021-01-05 08:24:15 -0500

    Add common implementation of contains_vertex(g,ukey)

--- include/graph/detail/graph_impl.hpp
+++ include/graph/detail/graph_impl.hpp
@@ -2,12 +2,18 @@
 #ifndef GRAPH_FWD_IMPL_HPP
 #define GRAPH_FWD_IMPL_HPP
 
-namespace std {
+// This file contains default implementations that will work in most/all cases
+// Individual graph structures can override if needed.
 
+namespace std {
 
 //
 // vertex Functions
 //
+template <directed_or_undirected G>
+constexpr bool contains_vertex(G const& g, vertex_key_t<G> ukey) noexcept {
+  return ukey >= 0 && ukey < static_cast<vertex_key_t<G>>(vertices_size(g));
+}
 
 
 } // namespace std
