9dccfeae75e1a279ccee30a72d245dee9e69e599
Andrew Lumsdaine <al75@uw.edu>
2021-01-13 12:40:05 -0800

    update docker build script

--- docker/build.bash
+++ docker/build.bash
@@ -4,6 +4,4 @@ for f in $*
 do
   docker build -f Dockerfile.${f}  -t ${f}  .  # --no-cache
 
-#  docker tag   ${f} registry.gitlab.com/al75/bgl17/${f}
-#  docker push  registry.gitlab.com/al75/bgl17/${f}
 done
